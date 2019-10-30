ec89143efb20559e5699b96e2a252109d58de942
Berk Karahan <berkkarahan00@gmail.com>
2019-10-30 23:37:26 +0300

    User serializer commit

---
+++ src/serializers/users.js
@@ -0,0 +1,42 @@
+class User {
+    tableFields = {
+        username: null,
+        email: null,
+        password: null,
+        firstname: null,
+        lastname: null,
+        phone: null,
+        address: null,
+        gender: ['male', 'female', 'other'],
+        birthday: null,
+        role: ['user', 'owner', 'admin'],
+        register_ip: null,
+        last_login_ip: null,
+        ts_login: null,
+        ts_register: null,
+        is_active: [0, 1],
+        is_verified: [0, 1]
+    }
+    constructor(row) {
+        Object.entries(row)
+            .forEach((keyValueTuple) => {
+                const key = keyValueTuple[0]
+                const value = keyValueTuple[1]
+                if (key in this.tableFields) {
+                    if (this.tableFields[key] === null) {
+                        this[key] = value
+                    } else {
+                        if (this.tableFields[key].indexOf(value) > -1) {
+                            this[key] = value
+                        } else {
+                            throw new Error("Value for key: " + key + " is not in default values.")
+                        }
+                    }
+                } else {
+                    throw new Error("Key: " + key + " is not in users table.")
+                }
+            })
+    }
+}
+
+export default User
