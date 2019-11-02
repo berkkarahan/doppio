01319582a2e0c338b9a47553e6e020dde24eaa62
berkkarahan <berkkarahan00@gmail.com>
2019-11-02 22:46:22 +0300

    Add mysql package
    Make mysql queries promise based.

---
+++ src/mysql.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql'
+// require('dotenv').config();
+
+const cn = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+};
+
+const connection = mysql.createConnection(cn);
+
+const executeQuery = async (query, connection) => {
+    return new Promise((resolve, reject) => {
+        connection.query(query, (err, data) => {
+            connection.end();
+            return err ? reject(err) : resolve(data);
+        });
+    });
+};
+
+export default {
+    connection,
+    executeQuery
+}
